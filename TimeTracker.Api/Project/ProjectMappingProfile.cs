90a18356e68f5fe5c07fe0e9242830b3cd608ac4
Danny Neupauer <danny.neupauer@t2informatik.de>
2023-12-13 16:53:16 +0100

    added ProjectMappingProfile

---
+++ TimeTracker.Api/Project/ProjectMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using AutoMapper;
+using TimeTracker.Api.Project.ViewModels;
+
+namespace TimeTracker.Api.Project;
+
+public class ProjectMappingProfile : Profile
+{
+    public ProjectMappingProfile()
+    {
+        CreateMap<Project, ProjectReadViewModel>();
+        CreateMap<Project, ProjectWriteViewModel>();
+        CreateMap<ProjectWriteViewModel, Project>();
+        CreateMap<ProjectReadViewModel, Project>();
+        CreateMap<ProjectReadViewModel, ProjectWriteViewModel>();
+    }
+}
