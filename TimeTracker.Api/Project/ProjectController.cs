d2c288d508b004f64910c68904b416ec6ad30846
Danny Neupauer <danny.neupauer@t2informatik.de>
2023-12-13 16:53:21 +0100

    added ViewModels to ProjectController

--- TimeTracker.Api/Project/ProjectController.cs
+++ TimeTracker.Api/Project/ProjectController.cs
@@ -1,54 +1,73 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TimeTracker.Api.Context;
+using TimeTracker.Api.Project.ViewModels;
 
 namespace TimeTracker.Api.Project;
 
 [ApiController]
 [Route("/projects")]
-public class ProjectController(TimeTrackerContext context) : ControllerBase
+public class ProjectController : ControllerBase
 {
+    private readonly TimeTrackerContext _context;
+    private readonly IMapper _mapper;
+
+    public ProjectController(TimeTrackerContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
 
     [HttpPost]
-    public async Task<ActionResult> Create(Project project)
+    public async Task<ActionResult> Create(ProjectWriteViewModel projectWriteViewModel)
     {
-        context.Add(project);
+        var project = _mapper.Map<Project>(projectWriteViewModel);
+        var projectEntity = _context.Add(project);
+        await _context.SaveChangesAsync();
+
+        var projectReadViewModel = _mapper.Map<ProjectReadViewModel>(projectEntity);
     
-        await context.SaveChangesAsync();
-    
-        return CreatedAtAction("GetById", new {id = project.Id}, project);
+        return CreatedAtAction("GetById", new {id = projectReadViewModel.Id}, projectReadViewModel);
     }
     
     [HttpGet]
     public async Task<ActionResult> GetAll()
     {
-        var projects = await context.Projects
+        var projects = await _context.Projects
             .ToListAsync();
-    
-        return Ok(projects);
+
+        var projectReadViewModels = _mapper.Map<List<ProjectReadViewModel>>(projects);
+        
+        return Ok(projectReadViewModels);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult> GetById(int id)
     {
-        var project = await context.Projects.FindAsync(id);
+        var project = await _context.Projects.FindAsync(id);
 
-        return project != null
-            ? Ok(project)
+        var projectReadViewModel = _mapper.Map<ProjectReadViewModel>(project);
+        
+        return projectReadViewModel != null
+            ? Ok(projectReadViewModel)
             : NotFound();
     }
 
     [HttpPut("{id}")]
-    public async Task<ActionResult> Edit(int id)
+    public async Task<ActionResult> Edit(int id, ProjectWriteViewModel projectWriteViewModel)
     {
-        var project = await context.Projects.FindAsync(id);
+        if (!await EntityExists(id)) return NotFound();
 
-        if (project is null) return NotFound();
+        var project = new Project()
+        {
+            Id = id,
+            Name = projectWriteViewModel.Name,
+            Customer = projectWriteViewModel.Customer
+        };
 
-        project.Name = project.Name;
-        project.Customer = project.Customer;
-
-        await context.SaveChangesAsync();
+        _context.Entry(project).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
 
         return NoContent();
     }
@@ -56,13 +75,18 @@ public class ProjectController(TimeTrackerContext context) : ControllerBase
     [HttpDelete("{id}")]
     public async Task<ActionResult> Delete(int id)
     {
-        var project = await context.Projects.FindAsync(id);
+        var project = await _context.Projects.FindAsync(id);
 
         if (project is null) return NotFound();
-        context.Remove(project);
+        _context.Projects.Remove(project);
 
-        await context.SaveChangesAsync();
+        await _context.SaveChangesAsync();
 
         return NoContent();
+    }
+    
+    private Task<bool> EntityExists(int id)
+    {
+        return _context.Projects.AnyAsync(p => p.Id == id);
     }
 }
