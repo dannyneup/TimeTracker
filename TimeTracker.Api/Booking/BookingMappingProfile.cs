eb95a3df377d75e7a63cd447ffe19a059319966b
Danny Neupauer <danny.neupauer@t2informatik.de>
2023-12-14 17:00:49 +0100

    added BookingMappingProfile

---
+++ TimeTracker.Api/Booking/BookingMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TimeTracker.Api.Booking.ViewModels;
+
+namespace TimeTracker.Api.Booking;
+
+public class BookingMappingProfile : Profile
+{
+    public BookingMappingProfile()
+    {
+        CreateMap<Booking, BookingReadViewModel>();
+        CreateMap<Booking, BookingWriteViewModel>();
+        CreateMap<BookingReadViewModel, Booking>();
+        CreateMap<BookingWriteViewModel, Booking>();
+        CreateMap<BookingReadViewModel, BookingWriteViewModel>();
+    }
+}
