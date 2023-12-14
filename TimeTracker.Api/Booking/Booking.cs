b3ef21067e854a594d5e2bd3b79c398d95d78200
Danny Neupauer <danny.neupauer@t2informatik.de>
2023-12-14 16:13:54 +0100

    Added Booking (View-)Models

---
+++ TimeTracker.Api/Booking/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TimeTracker.Api.Booking;
+
+[Table("Booking")]
+public class Booking
+{
+    [Key]
+    public required int Id { get; init; }
+    [Required]
+    public required Employee.Employee Employee { get; set; }
+    [Required]
+    public required Project.Project Project { get; set; }
+    
+    [Required]
+    public required DateTimeOffset Start { get; set; }
+    public DateTimeOffset End { get; set; }
+}
---
+++ TimeTracker.Api/Booking/ViewModels/BookingWriteViewModel.cs
@@ -0,0 +1,3 @@
+﻿namespace TimeTracker.Api.Booking.ViewModels;
+
+public record BookingWriteViewModel(int ProjectId, int EmployeeId, DateTimeOffset Start, DateTimeOffset End);
