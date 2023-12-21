a686223a2d21465520c82fc3a1df0f57703f552e
Danny Neupauer <danny.neupauer@t2informatik.de>
2023-12-21 18:33:24 +0100

    renamed generic Repository Types

--- TimeTracker.Api/Repositories/IRepository.cs
+++ TimeTracker.Api/Repositories/IRepository.cs
@@ -1,13 +1,13 @@
 ﻿namespace TimeTracker.Api.Repositories;
 
-public interface IRepository<T, TRequest, TResponse> 
+public interface IRepository<T, TWrite, TRead> 
     where T : class
-    where TRequest : class
-    where TResponse : class
+    where TWrite : class
+    where TRead : class
 {
-    Task<TResponse?> GetByIdAsync(int id);
-    IQueryable<TResponse> GetAll();
-    Task<TResponse> AddAsync(TRequest request);
-    Task UpdateAsync(int id, TRequest request);
+    Task<TRead?> GetByIdAsync(int id);
+    IQueryable<TRead> GetAll();
+    Task<TRead> AddAsync(TWrite request);
+    Task UpdateAsync(int id, TWrite request);
     Task DeleteAsync(int id);
 }
