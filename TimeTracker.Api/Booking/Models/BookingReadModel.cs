766a3ee02e6c6fbd0341cd6506a54fef5f9b1976
Danny Neupauer <danny.neupauer@t2informatik.de>
2023-12-22 13:17:29 +0100

    added Booking Read and WriteModels

---
+++ TimeTracker.Api/Booking/Models/BookingReadModel.cs
@@ -0,0 +1,3 @@
+﻿namespace TimeTracker.Api.Booking.Models;
+
+public record BookingReadModel(int Id, int ProjectId, int EmployeeId, DateTimeOffset Start, DateTimeOffset End);
---
+++ TimeTracker.Api/Booking/Models/BookingWriteModel.cs
@@ -0,0 +1,3 @@
+﻿namespace TimeTracker.Api.Booking.Models;
+
+public record BookingWriteModel(int ProjectId, int EmployeeId, DateTimeOffset Start, DateTimeOffset End);
